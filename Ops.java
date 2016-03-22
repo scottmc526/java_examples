17457b24813e48d96c04810ff6ba806a68986e49
Scott Christensen <scottmc526@gmail.com>
2016-03-22 11:25:52 -0600

    multiply method

--- Ops.java
+++ Ops.java
@@ -2,7 +2,11 @@ public class Ops {
   public static int sum(int x, int y){
     return x + y;
   }
+  public static int multiply(int x, int y, int z){
+    return x * y * z;
+  }
   public static void main(String[] args){
     System.out.println(sum(1,2));
+    System.out.println(multiply(2,2,2));
   }
 }
