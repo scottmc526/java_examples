f3e07772745fb538cf15c84360f5012b88f16681
Scott Christensen <scottmc526@gmail.com>
2016-03-22 11:55:45 -0600

    power

--- Ops.java
+++ Ops.java
@@ -5,8 +5,13 @@ public class Ops {
   public static int multiply(int x, int y, int z){
     return x * y * z;
   }
+  public static int power(int x){
+    int i = x;
+    return java.lang.Math.pow(x,i); 
+  }
   public static void main(String[] args){
     System.out.println(sum(1,2));
     System.out.println(multiply(2,2,2));
+    System.out.println(power(2));
   }
 }
